7bcf37c2ea43da8cd8b2bb8bb1e710e6a14a4b35
tildejustin <spacepepper3.14@gmail.com>
2023-02-17 19:14:18 -0500

    instawin working, now try sub tick?

--- src/main/java/xyz/tildejustin/rankedcheating/mixins/CheatingMixin.java
+++ src/main/java/xyz/tildejustin/rankedcheating/mixins/CheatingMixin.java
@@ -1,6 +1,7 @@
 package xyz.tildejustin.rankedcheating.mixins;
 
 import com.mcsr.projectelo.MCSREloProject;
+import com.redlimerl.speedrunigt.timer.InGameTimer;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.screen.CreditsScreen;
 import net.minecraft.network.packet.c2s.play.ClientStatusC2SPacket;
@@ -12,12 +13,14 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(MinecraftClient.class)
 public class CheatingMixin {
-    @Inject(method = "render", at = @At("TAIL"))
+    @Inject(method = "tick", at = @At("HEAD"))
     public void cheat(CallbackInfo ci) {
-        if (MCSREloProject.RUNNING_MATCH)
+        if (InGameTimer.getInstance().isStarted() && !InGameTimer.getInstance().isCompleted() && MCSREloProject.RUNNING_MATCH) {
             MinecraftClient.getInstance().openScreen(new CreditsScreen(true, () -> {
                 assert MinecraftClient.getInstance().player != null;
                 MinecraftClient.getInstance().player.networkHandler.sendPacket(new ClientStatusC2SPacket(ClientStatusC2SPacket.Mode.PERFORM_RESPAWN));
             }));
+            System.out.println("Attempted credits screen open");
         }
+    }
 }
